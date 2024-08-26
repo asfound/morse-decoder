b58f9740d7251d2808ddd27a037e983daef489a8
asfound <fffound27@gmail.com>
2024-08-26 21:07:03 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+      // slice expression by 10 into encoded letters
+  const encodedLetters = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    encodedLetters.push(expr.slice(i, i + 10));
+  }
+
+  // decode letters into morse code
+  const morseLetters = encodedLetters.map((item) => 
+    item
+      .replaceAll("10", ".")
+      .replaceAll("11", "-")
+      .replaceAll("00", "")
+      .replace("**********", " ")
+  );
+  
+  // decode letters from morse code
+  const decodedLetters = morseLetters.map(item => item === ' ' ? item : MORSE_TABLE[item]);
+
+  return decodedLetters.join('')
+
 }
 
 module.exports = {
